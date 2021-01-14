4a47105eb7728823d1df5c5a746371251ddeee7d
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-14 17:41:32 +0200

    Initial Front-end commit + Bootstrap setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Coffee Shop!</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Best Coffee Shop!</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
