978319d30a52b9245b067396e0ed4fbf3dfe816d
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-15 14:30:22 +0200

    Home Page initial commit && dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <h1>Best Coffee Shop!</h1>
-        </Container>
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <HomeScreen />
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
 };
 
 export default App;
