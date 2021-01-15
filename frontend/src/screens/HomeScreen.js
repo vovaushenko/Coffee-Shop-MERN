b76353e4d9396170f789efc20fdf3201617774b4
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-15 14:38:03 +0200

    Product Component initial commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import Product from '../components/Product';
 import products from '../products';
 
 const HomeScreen = () => {
