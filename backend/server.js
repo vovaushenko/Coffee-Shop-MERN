eccedbfbbd82f74b797b01414e315164e7d3a46a
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-17 20:58:26 +0200

    Connected frontend with backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -17,4 +20,7 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find((p) => req.params.id === p._id);
     res.json(product);
 });
-app.listen(5000, console.log('server running on port 5000 👋'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`👋 Server running in ${process.env.NODE_ENV} on port ${PORT} 👋`));
