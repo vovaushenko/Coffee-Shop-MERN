5ca01205c8f99dca71404bb1d4dd2b2a73d3536a
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-01 15:31:41 +0200

    Update Order to Delivered - backend endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 import {
   addOrderItems,
   getOrderById,
-  updateOrderToPaid,
-  getMyOrders,
   getOrders,
+  getMyOrders,
+  updateOrderToPaid,
+  updateOrderToDelivered,
 } from '../controllers/orderController.js';
 import { protect, isAdmin } from '..//middleware/authMiddleware.js';
 
@@ -13,5 +14,6 @@ router.route('/').post(protect, addOrderItems).get(protect, isAdmin, getOrders);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, isAdmin, updateOrderToDelivered);
 
 export default router;
