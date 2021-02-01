589a7663345bf380bbab994dd48f95b206735ef7
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-01 10:24:07 +0200

    Product Edit Screen - add image to a new product (frontend)

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -12,17 +12,16 @@ const storage = multer.diskStorage({
     cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
   },
 });
-
 // @desc check for proper file/mime types
 function checkFileType(file, cb) {
   const filetypes = /jpg|jpeg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLocaleLowerCase());
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
 
   if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb('Images Only!');
+    cb('Images only!');
   }
 }
 
@@ -36,5 +35,4 @@ const upload = multer({
 router.post('/', upload.single('image'), (req, res) => {
   res.send(`/${req.file.path}`);
 });
-
 export default router;
